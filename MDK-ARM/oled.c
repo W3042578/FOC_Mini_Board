4190e87574ec11df9f1b2ee5bb6209e279238ac4
W3042578 <3097000679@qq.com>
2023-09-20 15:51:47 +0800

    oled

--- MDK-ARM/oled.c
+++ MDK-ARM/oled.c
@@ -3,7 +3,14 @@
 #include "object_commicate.h"
 #include "i2c.h"
 
-//SSD1306 OLED驱动芯片IIC通讯显示屏幕
+uint8_t OLED_GRAM[128][8];//模拟SSD1306 OLED驱动芯片 参考：https://blog.csdn.net/weixin_43872149/article/details/104712248
 
+//SSD1306 OLED驱动芯片IIC通讯显示屏幕
+uint8_t OLED_Control_Send(void)
+{
+
+
+
+}
 
 
--- MDK-ARM/oled.h
+++ MDK-ARM/oled.h
@@ -1,8 +1,8 @@
 #ifndef __OLED_H
 #define __OLED_H
+#include "stdint.h"
 
-
-
+extern uint8_t OLED_GRAM[128][8];
 
 
 
