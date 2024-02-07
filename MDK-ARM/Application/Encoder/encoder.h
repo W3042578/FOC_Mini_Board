aa423898466ba4ef434ad2a0dc4d0edb774f388f
乘风邀影 <103897766+W3042578@users.noreply.github.com>
2024-02-07 16:15:19 +0800

    Update encoder.h

--- MDK-ARM/Application/Encoder/encoder.h
+++ MDK-ARM/Application/Encoder/encoder.h
@@ -14,15 +14,9 @@ typedef struct _Encoder
 	uint16_t	Encoder_Angle_Buffer;			//编码器上一次值
 	int32_t		Encode_Position;			//编码器位置数据，包含多圈计数
 	int16_t 	Encoder_Speed_Angle;			//1ms编码器速度
-	int16_t 	Encoder_Speed_Angle_Buffer;		//上一次1ms编码器速度
-	int16_t 	Encoder_Speed_Angle_Loop;		//速度环编码器速度
-	int16_t 	Encoder_Speed_Angle_Loop_Buffer;	//上一次速度环编码器速度
-	int16_t 	Speed_Filter;				//速度滤波控制位
-	int16_t		Last_Encoder_Loop;			//速度环内部计算速度用
-	int16_t 	Speed_Filter_Loop;              	//速度环速度计算滤波
+	int16_t 	Speed_Filter;				//编码器速度滤波控制位
 	uint32_t 	Encoder_Offest_Data;		        //编码器修正值
 	uint8_t		Encoder_Direction;			//编码器方向，与q轴方向相同为0，相反为1
-	uint16_t 	Encoder_Direction_Position;		//编码器方向判断用位置
 }Encoder;
 
 extern  uint16_t Tx_Encoder[2];
