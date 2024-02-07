70e9f89f6ddb4a10843a455e914799c14212dfdd
乘风邀影 <103897766+W3042578@users.noreply.github.com>
2024-02-07 14:24:39 +0800

    Update control_loop.c

--- MDK-ARM/Algorithm/control_loop.c
+++ MDK-ARM/Algorithm/control_loop.c
@@ -11,103 +11,103 @@ _PID_Control	Current_Q_PID,Current_D_PID,Speed_PI,Position_PI;
 //并联式PID控制  
 //参考https://blog.csdn.net/qq_38833931/article/details/80630960
 //实际只用PI 并联式抗积分饱和
-void PID_Control_Deal(_PID_Control * PID_Control)
+void PID_Control_Deal(_PID_Control * pid_control)
 {
 	int32_t Integral_Data;	//积分结果变量
 	int32_t	Init_Output;	//未限幅前总输出
 	int32_t Anti_Wind;	//抗饱和结果变量
 	
-	PID_Control->Error = PID_Control->Expect - PID_Control->Feedback;
-	PID_Control->Integral_Sum = PID_Control->Integral_Sum + PID_Control->Error;
+	pid_control->Error = pid_control->Expect - pid_control->Feedback;
+	pid_control->Integral_Sum = pid_control->Integral_Sum + pid_control->Error;
 
-	PID_Control->Proportion_Sum = PID_Control->Proportion * PID_Control->Error;   //Kp * Error
-	Integral_Data = PID_Control->Integral * PID_Control->Integral_Sum;		//Ki * Integral
-	PID_Control->Difference_Sum = PID_Control->Difference * (PID_Control->Error - PID_Control->Last_Error); //Kd * Difference
+	pid_control->Proportion_Sum = pid_control->Proportion * pid_control->Error;   //Kp * Error
+	Integral_Data = pid_control->Integral * pid_control->Integral_Sum;		//Ki * Integral
+	pid_control->Difference_Sum = pid_control->Difference * (pid_control->Error - pid_control->Last_Error); //Kd * Difference
 	//比例限幅
-	if(PID_Control->Proportion_Sum > PID_Control->Proportion_Limit)
-		PID_Control->Proportion_Sum = PID_Control->Proportion_Limit;
-	else if(PID_Control->Proportion_Sum < -PID_Control->Proportion_Limit)
-		PID_Control->Proportion_Sum = -PID_Control->Proportion_Limit;
+	if(pid_control->Proportion_Sum > pid_control->Proportion_Limit)
+		pid_control->Proportion_Sum = pid_control->Proportion_Limit;
+	else if(pid_control->Proportion_Sum < -pid_control->Proportion_Limit)
+		pid_control->Proportion_Sum = -pid_control->Proportion_Limit;
 	
 	//积分限幅
-	if(Integral_Data > PID_Control->Integral_Limit)
-		Integral_Data = PID_Control->Integral_Limit;
-	else if(Integral_Data < -PID_Control->Integral_Limit)
-		Integral_Data = PID_Control->Integral_Limit;
+	if(Integral_Data > pid_control->Integral_Limit)
+		Integral_Data = pid_control->Integral_Limit;
+	else if(Integral_Data < -pid_control->Integral_Limit)
+		Integral_Data = pid_control->Integral_Limit;
 	//微分限幅
-	if(PID_Control->Difference_Sum > PID_Control->Difference_Limit)
-		PID_Control->Difference_Sum = PID_Control->Difference_Limit;
-	else if(PID_Control->Difference_Sum < -PID_Control->Difference_Limit)
-		PID_Control->Difference_Sum = -PID_Control->Difference_Limit;
+	if(pid_control->Difference_Sum > pid_control->Difference_Limit)
+		pid_control->Difference_Sum = pid_control->Difference_Limit;
+	else if(pid_control->Difference_Sum < -pid_control->Difference_Limit)
+		pid_control->Difference_Sum = -pid_control->Difference_Limit;
 	
 	//总输出限幅并计算抗饱和
-	PID_Control->Output_Sum = PID_Control->Proportion_Sum + Integral_Data + PID_Control->Difference_Sum;
-	Init_Output = PID_Control->Output_Sum;
-	if(PID_Control->Output_Sum > PID_Control->Output_limit)
-		PID_Control->Output_Sum = PID_Control->Output_limit;
-	else if(PID_Control->Output_Sum < -PID_Control->Output_limit)
-		PID_Control->Output_Sum = -PID_Control->Output_limit;
-	Anti_Wind = PID_Control->Antiback * (Init_Output - PID_Control->Output_Sum);
+	pid_control->Output_Sum = pid_control->Proportion_Sum + Integral_Data + pid_control->Difference_Sum;
+	Init_Output = pid_control->Output_Sum;
+	if(pid_control->Output_Sum > pid_control->Output_limit)
+		pid_control->Output_Sum = pid_control->Output_limit;
+	else if(pid_control->Output_Sum < -pid_control->Output_limit)
+		pid_control->Output_Sum = -pid_control->Output_limit;
+	Anti_Wind = pid_control->Antiback * (Init_Output - pid_control->Output_Sum);
 	
 	//积分有限幅 控制抗饱和反馈避免积分量正负间跳变
-	if(((Anti_Wind > 0) && (Anti_Wind < PID_Control->Integral_Sum)) || ((Anti_Wind < 0) && (Anti_Wind > PID_Control->Integral_Sum)))
-		PID_Control->Integral_Sum = PID_Control->Integral_Sum - Anti_Wind;
+	if(((Anti_Wind > 0) && (Anti_Wind < pid_control->Integral_Sum)) || ((Anti_Wind < 0) && (Anti_Wind > pid_control->Integral_Sum)))
+		pid_control->Integral_Sum = pid_control->Integral_Sum - Anti_Wind;
 	else
-		PID_Control->Integral_Sum = 0;
+		pid_control->Integral_Sum = 0;
 }
 
 //PID结构体参数初始化
-void PID_Control_Init(_PID_Control *PID)
+void PID_Control_Init(_PID_Control *pid)
 {
-	PID->Proportion = 0;
-	PID->Integral = 0;
-	PID->Difference = 0;
-	PID->Antiback = 0;
+	pid->Proportion = 0;
+	pid->Integral = 0;
+	pid->Difference = 0;
+	pid->Antiback = 0;
 	
-	PID->Error = 0;
-	PID->Last_Error = 0;
-	PID->Feedback = 0;
-	PID->Expect = 0;
-	PID->Feedforward = 0;
+	pid->Error = 0;
+	pid->Last_Error = 0;
+	pid->Feedback = 0;
+	pid->Expect = 0;
+	pid->Feedforward = 0;
 	
-	PID->Proportion_Sum = 0;
-	PID->Integral_Sum = 0;
-	PID->Difference_Sum = 0;
-	PID->Output_Sum = 0;
+	pid->Proportion_Sum = 0;
+	pid->Integral_Sum = 0;
+	pid->Difference_Sum = 0;
+	pid->Output_Sum = 0;
 	
-	PID->Proportion_Limit = 0;
-	PID->Integral_Limit = 0;
-	PID->Difference_Limit = 0;
+	pid->Proportion_Limit = 0;
+	pid->Integral_Limit = 0;
+	pid->Difference_Limit = 0;
 }
 
 //PID控制环具体参数初始化
-void Control_Loop_Init(_Control_Loop *Loop)
+void Control_Loop_Init(_Control_Loop *loop)
 {
-	Loop->Target_Q_Current = 0;
-	Loop->Target_D_Current = 0;
-	Loop->Target_Speed = 0;
-	Loop->Target_Position = 0;
-	Loop->Loop_Count = 0;
+	loop->Target_Q_Current = 0;
+	loop->Target_D_Current = 0;
+	loop->Target_Speed = 0;
+	loop->Target_Position = 0;
+	loop->Loop_Count = 0;
 	
-	Loop->Current_Q_Proportion = 50;
-	Loop->Current_Q_Integral = 1;
-	Loop->Current_Q_Difference = 0;
+	loop->Current_Q_Proportion = 50;
+	loop->Current_Q_Integral = 1;
+	loop->Current_Q_Difference = 0;
 	
-	Loop->Current_D_Proportion = 50;
-	Loop->Current_D_Integral = 1;
-	Loop->Current_D_Difference = 0;
+	loop->Current_D_Proportion = 50;
+	loop->Current_D_Integral = 1;
+	loop->Current_D_Difference = 0;
 	
-	Loop->Speed_Proportion = 25;
-	Loop->Speed_Integral = 1;
+	loop->Speed_Proportion = 25;
+	loop->Speed_Integral = 1;
 
-	Loop->Position_Proportion = 10;
-	Loop->Position_Integral = 0;
-	Loop->Position_Feedforward = 1;
+	loop->Position_Proportion = 10;
+	loop->Position_Integral = 0;
+	loop->Position_Feedforward = 1;
 	
-	Loop->Current_Q_Output_Limit = 1.5 * 2048;
-	Loop->Current_D_Output_Limit = 1 * 2048;
-	Loop->Speed_Output_Limit = 0;
-	Loop->Position_Output_Limit = 0;
+	loop->Current_Q_Output_Limit = 1.5 * 2048;
+	loop->Current_D_Output_Limit = 1 * 2048;
+	loop->Speed_Output_Limit = 0;
+	loop->Position_Output_Limit = 0;
 }
 
 //1ms中断更新PID结构体数据
