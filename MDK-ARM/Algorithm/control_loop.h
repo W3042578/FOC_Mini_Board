74f919c8eaf85e16190fbe4a1e39e810d5269aca
乘风邀影 <103897766+W3042578@users.noreply.github.com>
2024-02-07 13:25:12 +0800

    Update control_loop.h

--- MDK-ARM/Algorithm/control_loop.h
+++ MDK-ARM/Algorithm/control_loop.h
@@ -6,28 +6,28 @@
 
 typedef  struct
 {
-	int32_t 	Target_Q_Current;			//目标 
+	int32_t 	Target_Q_Current;	//目标 
 	int32_t 	Target_D_Current;
 	int32_t 	Target_Speed;								
 	int32_t 	Target_Position;
-	
+	uint8_t		Loop_Count;		//环路计数，内环运行频率必须大于外环，否则环路震荡
 
-	uint16_t	Current_Q_Proportion;			//q轴电流PID
+	uint16_t	Current_Q_Proportion;	//q轴电流PID
 	uint16_t	Current_Q_Integral;
 	uint16_t	Current_Q_Difference;
 	
-	uint16_t	Current_D_Proportion;			//d轴电流PID
+	uint16_t	Current_D_Proportion;	//d轴电流PID
 	uint16_t	Current_D_Integral;
 	uint16_t	Current_D_Difference;
 
-	uint16_t	Speed_Proportion;					//速度PI
+	uint16_t	Speed_Proportion;	//速度PI
 	uint16_t	Speed_Integral;
 		
-	uint16_t	Position_Proportion;			//位置PI
+	uint16_t	Position_Proportion;	//位置PI
 	uint16_t	Position_Integral;		
 	uint16_t 	Position_Feedforward;	
 	
-	int32_t  	Current_Q_Output_Limit;		//输出限制
+	int32_t  	Current_Q_Output_Limit;	//输出限制
 	int32_t  	Current_D_Output_Limit;	
 	int32_t		Speed_Output_Limit;
 	int32_t		Position_Output_Limit;
