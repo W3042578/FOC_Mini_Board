27a6cb302c2d53ef12a10cf6a3455bdbd12ea959
乘风邀影 <103897766+W3042578@users.noreply.github.com>
2024-03-18 13:24:28 +0800

    Update basic_function.c

--- MDK-ARM/Application/Function/basic_function.c
+++ MDK-ARM/Application/Function/basic_function.c
@@ -492,7 +492,14 @@ void Current_Forward_Feedback(FOC_Motor *motor)
 void Dead_Time_Compensate(FOC_Motor *motor)
 {
 	//计算合成电流Is在静态坐标下离alpha轴角度，根据角度判断三相电流极性，再根据极性设置补偿时间增减
-	
+	//电流极性扇区判断
+	uint8_t	dead_sector;
+
+	switch(dead_sector)
+	{
+		case 1:
+		
+	}
 	
 }
 
