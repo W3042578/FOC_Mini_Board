3ebfd156539091abe41f9eb84f8fd4cbb4b492ee
乘风邀影 <103897766+W3042578@users.noreply.github.com>
2024-03-19 14:26:35 +0800

    Update filter.h

--- MDK-ARM/Application/Function/filter.h
+++ MDK-ARM/Application/Function/filter.h
@@ -7,7 +7,10 @@
 #define sample_freq		2
 #define	cutoff_freq		3
 
+//滑动滤波器
+void Sliding_Filter(int16 * data_in,int16 * data_out,uint8 sliding_rate);
 
+//巴特沃斯低通滤波器
 int32_t Butterworth_Second(int32_t data);
 
 
